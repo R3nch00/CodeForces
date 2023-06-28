5ded5463431c1012aa409ff5723c9719bda30dbf
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-06-29 00:36:04 +0600

    Add files via upload

---
+++ Laptops.c
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a; cin>>a;
+    for(int i=0;i<a;i++){int x,y; cin>>x>>y;
+        if(x!=y){cout<<"Happy Alex"; return 0;} } cout<<"Poor Alex";
+}
