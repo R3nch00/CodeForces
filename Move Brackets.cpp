66da32e7e7f26dd9407152383138bcb7cb6aac88
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-07-01 02:08:59 +0600

    Add files via upload

---
+++ Move Brackets.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int t,n,l,r,x,y; char c; cin>>t;
+while(t--){cin>>n,l=0,r=0;
+while (n--) cin>>c,x=c=='('?1:0,r+=x,y=r>0&&!x?1:0,r-=y,l+=y;
+cout<<r<<endl;}}
