f5e3486b60bdcea438c5866e1827b1f0112c7f40
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-07-01 23:45:34 +0600

    Add files via upload

---
+++ Nastia and Nearly Good Numbers.cpp
@@ -0,0 +1,6 @@
+#import<bits/stdc++.h>
+using namespace std;
+int main(){
+long long t,a,b; cin>>t;
+while(t--){
+        cin>>a>>b; b-1 ? cout<<"YES\n"<<a<<" "<<a*b<<" "<<a+(a*b)<<endl : cout<<"NO\n";}}
