1c1b5adb527d1c5a3b899dfcb24aabdb925abb34
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-07-01 23:27:40 +0600

    Add files via upload

---
+++ Olesya and Rodion.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+long long n,t; cin>>n>>t;
+if(n==1&&t==10){cout<<-1; return 0;}
+if(t==10){cout<<1;}
+else cout<<t;
+for(int i=1;i<n;++i){
+    cout<<0; }
+return 0;}
