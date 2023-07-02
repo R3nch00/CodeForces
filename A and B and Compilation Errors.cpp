d5c49351c10a9c96934f7c59e199aea6625c69ef
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-07-02 23:39:15 +0600

    Add files via upload

---
+++ A and B and Compilation Errors.cpp
@@ -0,0 +1,8 @@
+#import<bits/stdc++.h>
+using namespace std;
+int a[3],n,m,i,j;
+main() {
+    cin>>n;
+    for(i=0;i<3;i++)
+        for(j=0;j<n-i;j++) cin>>m,a[i]+=m;
+    cout<<a[0]-a[1]<<endl<<a[1]-a[2]; }
