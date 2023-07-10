ad3d48458db4bc0929f971ce93118c7111dfba03
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-07-11 01:27:21 +0600

    Add files via upload

---
+++ Plus or Minus.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){ int a,b,c; cin>>a>>b>>c;
+    (a+b == c) ? cout << "+\n" : cout << "-\n"; }
+int main(){
+    int t; cin>>t;
+    while(t--){ solve(); }
+return 0;}
