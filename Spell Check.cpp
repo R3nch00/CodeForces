05d38edd16522fe2b304ccfb071f6bc66c0a736b
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-07-11 13:53:09 +0600

    Add files via upload

---
+++ Spell Check.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t; cin>>t;
+	while(t--){ int n; cin>>n; string s; cin>>s;
+		sort(s.begin(),s.end());
+		cout<<(s=="Timru" ? "Yes\n": "No\n"); }}
