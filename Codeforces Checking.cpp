d80383b0f2fa7c8f3d76aab4f1555ebeab431603
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-07-22 21:34:19 +0600

    Add files via upload

---
+++ Codeforces Checking.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t; cin>>t;
+while(t--){ char c; cin>>c;
+   puts((c=='c'||c=='o'||c=='d'||c=='e'||c=='f'||c=='r'||c=='s') ? "YES\n" : "NO\n"); }}
