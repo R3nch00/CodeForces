bc7a8d7f5087d5353c5d9c1f4e3e74e691e30e6a
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-07-28 20:21:05 +0600

    Add files via upload

---
+++ Polycarp and Sums of Subsequences.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+long n,a,b,c,d,e,f,g;
+int main(){
+cin>>n;
+while(n--){
+   cin>>a>>b>>c>>d>>e>>f>>g;
+   cout<<g-f<<" "<<g-e<<" "<<f+e-g<<'\n';
+ }
+}
