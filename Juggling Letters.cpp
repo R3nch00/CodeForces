93af1bd9fba49d7e9e67da107a6fe40101899295
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-01 23:43:26 +0600

    Add files via upload

---
+++ Juggling Letters.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t; cin>>t;
+	while(t--){
+		int n; cin>>n;
+		map<char,int> m;
+		for(int i=0;i<n;++i){
+			string s; cin>>s;
+			for(char c : s)
+				m[c]++;
+		}
+		bool flag=1;
+		for(pair<char,int> e : m)
+			flag &= e.second % n == 0;
+		cout << (flag ? "Yes\n" : "No\n");
+	}
+}
