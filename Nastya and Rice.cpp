564b8f20d0e266591ddca1e76910419627360c3f
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-01 23:57:20 +0600

    Add files via upload

---
+++ Nastya and Rice.cpp
@@ -0,0 +1,8 @@
+#import <bits/stdc++.h>
+using namespace std;
+main(){
+int t,n,a,b,c,d;
+    for(cin>>t;t--;){
+        cin>>n>>a>>b>>c>>d;
+    puts(((a-b)*n>c+d || (a+b)*n<c-d) ? "No" : "Yes");
+}}
