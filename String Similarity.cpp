48b400efcc77437c3af4b8f9308e9e0daf9b5df6
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-05 00:55:16 +0600

    Add files via upload

---
+++ String Similarity.cpp
@@ -0,0 +1,11 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,t; string s; cin>>t;
+    while(t--){
+    cin>>n>>s;
+    for(int i=0;i<n;i++){ cout<<s[n-1]; }
+    cout<<endl;
+    }
+}
