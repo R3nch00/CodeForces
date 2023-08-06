0de0903773fc43b9b17e288d0ce4682fbfda98bf
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-07 00:47:01 +0600

    Add files via upload

---
+++ PizzaForces.cpp
@@ -0,0 +1,10 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    int t; cin>>t;
+while(t--){
+    ll n; cin>>n;
+    cout<<(max(6LL,n+1)/2*5)<<endl;}
+}
