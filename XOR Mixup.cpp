62ea8f0c10f2d1135faac06826ef06e01cfc28ad
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-07 00:11:03 +0600

    Add files via upload

---
+++ XOR Mixup.cpp
@@ -0,0 +1,11 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t; cin>>t;
+	while(t--){
+		int n; cin>>n; int a[n];
+		for(int i=0;i<n;i++) cin>>a[i];
+		cout<<a[0]<<endl;
+	}
+}
