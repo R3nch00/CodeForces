109d666af949475e9113b2b30bcc7eeffe4a4954
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-11 23:18:09 +0600

    Add files via upload

---
+++ Two Regular Polygons.cpp
@@ -0,0 +1,8 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    int t,m,n;
+    for(cin>>t;t--;){ cin>>m>>n; cout<<(m%n==0 ? "YES\n" : "NO\n"); }
+}
