92ddd3bd2c884a8bad4577205559de604f0d80dc
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-11 23:03:45 +0600

    Add files via upload

---
+++ Password.cpp
@@ -0,0 +1,12 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int main(){
+ 	int t; cin>>t;
+ 	while(t){
+ 		t--;
+ 		int n,a; cin>>n;
+ 		for(int i=1;i<=n;i++) cin>>a;
+ 		cout<<3*(10-n)*(9-n)<<endl;
+	 }
+}
