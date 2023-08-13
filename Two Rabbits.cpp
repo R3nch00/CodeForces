ac62646f7eb3392ff53f039892573c4f4d3a664c
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-13 23:30:24 +0600

    Add files via upload

---
+++ Two Rabbits.cpp
@@ -0,0 +1,11 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t,x,y,a,b; cin>>t;
+	while(t--){
+		cin>>x>>y>>a>>b;
+		if((y-x)%(a+b)==0) cout<<(y-x)/(a+b)<<endl;
+		else cout<<-1<<endl;
+	}
+}
