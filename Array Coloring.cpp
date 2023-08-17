c7a5ec350fbbc5e265122c7ab2f2e0c28539889c
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-18 01:07:37 +0600

    Add files via upload

---
+++ Array Coloring.cpp
@@ -0,0 +1,10 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int main(){
+  int t,n,s,a; cin>>t;
+  while(t--){
+    for(cin>>n,s=0;n--;s+= a) cin>>a;
+    cout<<(s&1 ? "NO\n" : "YES\n");
+  }
+}
