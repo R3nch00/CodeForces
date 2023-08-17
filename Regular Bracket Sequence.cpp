60e496307935c72622be30ce6600a465941babbe
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-18 01:10:09 +0600

    Add files via upload

---
+++ Regular Bracket Sequence.cpp
@@ -0,0 +1,8 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int main(){
+    int tc; cin>>tc;
+while(tc--){ string s,t; cin>>s;
+cout<<(s.size()%2||s[0]==')'||s.back()=='(' ? "NO\n" : "YES\n"); }
+}
