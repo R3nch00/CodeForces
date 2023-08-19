f53bf69b9269600050fda6b706f147e1bb9b1f75
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-19 23:45:32 +0600

    Add files via upload

---
+++ Suborrays.cpp
@@ -0,0 +1,12 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int t,n;
+int main(){
+	cin>>t;
+	while(t--){
+		cin>>n;
+		for(int i=1;i<=n;i++) cout<<i<<" "; cout<<endl;
+	}
+	return 0;
+}
