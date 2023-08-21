dcf9c14ec610154287f76feee116fc55ffb7cd5c
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-21 14:22:13 +0600

    Add files via upload

--- Effective Approach.cpp
+++ Effective Approach.cpp
@@ -2,13 +2,14 @@
 #import <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n,m,t; cin>>n>>m;
-    vector<int> v(n+1);
-    for(int i=1;i<=n;i++){  cin>>t; v[t]=i; }
-    long long t1(0),t2(0);
-    while(m--){
-        cin>>t; t1+=v[t]; t2+=(n-v[t]+1);
+    int n,x,m; cin>>n; int a[n];
+    for(int i=1; i<=n; i++){
+        cin>>x; a[x]=i;
     }
-    cout<<t1<<" "<<t2;
+    cin>>m; long long s1=0,s2=0;
+    while(m--){
+        cin>>x; s1+=a[x]; s2+=n-a[x]+1;
+    }
+    cout<<s1<<' '<<s2<<'\n';
     return 0;
 }
