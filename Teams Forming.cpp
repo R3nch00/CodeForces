582f0b5cb41179b397904555a95ab5a3c5b0316d
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-21 14:31:08 +0600

    Add files via upload

---
+++ Teams Forming.cpp
@@ -0,0 +1,14 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,ans=0; cin>>n; int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+sort(arr,arr+n);
+for(int i=0;i<n;i+=2){
+    ans+=arr[i+1]-arr[i];
+}
+cout<<ans;
+}
