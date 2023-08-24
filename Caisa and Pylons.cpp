62a7a96a70aee946c18e63144973b6cca3940420
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-24 23:57:52 +0600

    Add files via upload

---
+++ Caisa and Pylons.cpp
@@ -0,0 +1,11 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin>>n; int mx=0;
+    for(int i=0;i<n;i++){
+        int x; cin>>x;
+        mx=max(mx,x);
+    }
+    cout<<mx;
+}
