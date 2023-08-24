f422532e18d6b46cce1e1896a5e756fb95dc52cd
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-24 23:41:13 +0600

    Add files via upload

---
+++ Little Artem.cpp
@@ -0,0 +1,16 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+void NISHI(){
+    int n,m; cin >n>>m;
+    string black_row(m,'B');
+    vector<string> result(n, black_row);
+    result[0][0] = 'W';
+    for (int i = 0; i < n; ++i){
+        cout<<result[i]<<'\n';
+    }
+}
+int main(){
+    int t; cin>>t;
+    while(t--) NISHI();
+}
