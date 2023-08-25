7dc68df4ae9cd2c1903a2e6d7b97b808216a79d5
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-08-25 16:24:31 +0600

    Add files via upload

---
+++ Square Counting.cpp
@@ -0,0 +1,11 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+int main(){
+	int T; cin>>T;
+	while(T--){
+		long long  n,s; cin>>n>>s;
+		cout<<s/n/n<<endl;
+	}
+return 0;
+}
