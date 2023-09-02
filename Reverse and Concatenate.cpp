e4a8d538f5d937729c21d6ee8b5f383ffd9bbf98
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-09-03 01:03:49 +0600

    Add files via upload

---
+++ Reverse and Concatenate.cpp
@@ -0,0 +1,9 @@
+// mes que un club //
+#import <bits/stdc++.h>
+using namespace std;
+    int t,n,k; string s,r;
+int main(){
+    cin>>t;
+while(t--&&cin>>n>>k>>s)
+r=s,reverse(s.begin(),s.end()),puts(!k||s==r ? "1" : "2");
+}
