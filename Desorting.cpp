ae94711920b63c9c9a2caf7b625ae7ccf4158919
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-09-11 00:22:01 +0600

    Add files via upload

---
+++ Desorting.cpp
@@ -0,0 +1,13 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+  int t,n;
+  for(cin>>t; cin>>n; ){
+    int a,b,mn=1e9; cin>>a;
+    for(int i=1;i<n;++i)
+      cin>>b, mn=min(mn,b-a),a=b;
+    cout<< max(0,(mn+2)/2) <<endl;
+    }
+}
