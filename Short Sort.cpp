23bc7de8c466d51f482b01fb5256217ca5b1808c
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-10-12 21:53:28 +0600

    Add files via upload

---
+++ Short Sort.cpp
@@ -0,0 +1,11 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+int t; cin>>t;
+while(t--){
+string x; cin>>x;
+cout<<((x=="cab" ||x=="bca") ? "NO" : "YES")<<endl;
+    }
+}
