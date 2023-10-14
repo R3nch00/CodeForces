733d8cecb779be785cbe9b2605496f2fb98dc7fe
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-10-14 23:58:49 +0600

    Add files via upload

---
+++ The Ultimate Square.cpp
@@ -0,0 +1,9 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define ll long long
+#define f(n) for(int i=0;i<n;i++)
+using namespace std;
+int main(){
+    int c,n,x; cin>>n;
+    f(n){ cin>>x; cout<<(x+1)/2<<endl; }
+}
