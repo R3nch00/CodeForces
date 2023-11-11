91ad4720e0f3f5c8d23a89643c87e297263e9c55
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-11-12 00:52:27 +0600

    Add files via upload

---
+++ Subtraction Game.cpp
@@ -0,0 +1,9 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define ll long long
+#define f(n) for(int i=0;i<n;i++)
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while(t--){ int a,b; cin>>a>>b; cout<<a+b<<endl; }
+}
