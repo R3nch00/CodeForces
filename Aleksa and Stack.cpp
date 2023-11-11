27767889b456af4d76c2997a801c781381e91259
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-11-11 23:42:50 +0600

    Add files via upload

---
+++ Aleksa and Stack.cpp
@@ -0,0 +1,12 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define ll long long
+#define f(n) for(int i=0;i<n;i++)
+using namespace std;
+int main(){
+	int t; cin>>t;
+	while(t--){
+		int n; cin>>n;
+		f(n) cout<<i*2+1<<" "<<'\n';
+	}
+}
