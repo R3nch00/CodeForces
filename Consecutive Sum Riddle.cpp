fa42e7221ee5b8737f8da7e429b817952e91e007
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-11-12 00:05:55 +0600

    Add files via upload

---
+++ Consecutive Sum Riddle.cpp
@@ -0,0 +1,13 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define ll long long
+#define f(n) for(int i=0;i<n;i++)
+using namespace std;
+int main(){
+int t; ll int n; cin>>t;
+while(t--){
+    cin>>n;
+    cout<<(-n+1)<<" "<<n<<'\n';
+    }
+return 0;
+}
