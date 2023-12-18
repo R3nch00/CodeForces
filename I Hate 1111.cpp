34acb36a86faa2ea1219c9b3a05551454a0b1c6c
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-12-19 00:46:52 +0600

    Add files via upload

---
+++ I Hate 1111.cpp
@@ -0,0 +1,10 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define f(T)  for (int x; cin >> x; cout << (x >= 111 * (x % 11) ? "YES\n" : "NO\n"), T--);
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T; f(T);
+    return 0;
+}
