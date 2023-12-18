6fa5b71f15d953bbf96af0a47bc428f70180dc9e
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-12-19 00:27:44 +0600

    Add files via upload

---
+++ Game with Integers.cpp
@@ -0,0 +1,10 @@
+// mes que un club //
+#import <bits/stdc++.h>
+#define ll long long
+#define f(n) for(int i=0;i<n;i++)
+#define f(m) for(int i=0;i<m;i++)
+using namespace std;
+signed main(){
+int t; cin>>t;
+    while(t--){int n; cin>>n; cout<<((n%3)==0 ? "Second\n": "First\n");}
+}
