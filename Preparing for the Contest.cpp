cc9388f8ad48a05f9634876bcc12ee2ddf6a4308
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-01-05 02:12:17 +0600

    Add files via upload

---
+++ Preparing for the Contest.cpp
@@ -0,0 +1,14 @@
+// mes que un club //
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t; cin>>t;
+	while(t--){
+		int n,k; cin>>n>>k;
+		for(int i=n-k;i<=n;i++){
+			cout<<i<<" ";
+		}
+		for(int i=n-k-1;i>0;i--) cout<<i<<" ";
+		cout<<endl;
+	}
+}
