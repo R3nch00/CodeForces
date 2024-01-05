ad0d3c11292c2913b10733086e517d61f23345c9
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-01-06 03:47:24 +0600

    Add files via upload

--- Don't Try to Count.cpp
+++ Don't Try to Count.cpp
@@ -2,14 +2,15 @@
 #include <bits/stdc++.h>
 #define f(n) for(int i=0;i<n;i++)
 using namespace std;
-int main() {
+int main(){
 	int t,n,m; cin>>t;
 	while (t--) {
         cin>>n>>m;
 		string x,s; cin>>x>>s;
 		bool ok = false;
+		size_t len=x.size();
 		f(6){
-			if(x.find(s)!=-1) {
+			if(x.find(s)!= string::npos){
 				ok = true;
 				cout<<i<<endl;
 				break;
@@ -17,5 +18,6 @@ int main() {
 			x+=x; // Update the being searched value of x
 		}
 		if(!ok) cout<<-1<<endl;
-	}
+    }
+    return 0;
 }
