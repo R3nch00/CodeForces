f2654dac4dece05189d5a4b5f4520f08dbb45966
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-01-07 02:01:29 +0600

    Add files via upload

---
+++ Coins.cpp
@@ -0,0 +1,11 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+	int t; cin>>t;
+	while(t--){ ll n,k; cin>>n>>k;
+    cout<<((n%2==1 && k%2==0) ? "NO\n" : "YES\n");
+	}
+ return 0;
+}
---
+++ Watermelon.cpp
@@ -0,0 +1,5 @@
+// mes que un club //
+#include <bits/stdc++.h>
+using namespace std;
+main(int n){
+    cin>>n; puts(n<4|n&1 ? "NO":"YES"); }
