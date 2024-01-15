a8d21a781661d4389d959b59fbd417f00b3cd97f
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-01-16 02:39:22 +0600

    Add files via upload

---
+++ GCD vs LCM.cpp
@@ -0,0 +1,14 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#define ll long long
+#define ar array
+#define f(n) for(int i{};i<n;i++)
+#define f(k) for(int i{};i<=k;i++)
+using namespace std;
+int main(){
+    int n; cin>>n;
+    while(n--){ int a; cin>>a;
+        cout<<a-3<<" 1 1 1"<<endl;
+    }
+     return 0;
+}
