2d9fc8f865842768f768fca1687bc054a00c8eda
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-01-16 02:42:08 +0600

    Add files via upload

---
+++ Dima and Friends.cpp
@@ -0,0 +1,14 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#define ll long long
+#define ar array
+#define f(n) for(int i{};i<n;i++)
+#define f(k) for(int i{};i<=k;i++)
+int i,k,n,s,t;
+using namespace std;
+int main(){
+    for(cin>>n;i++<n;s+=k) cin>>k;
+    for(i=6;--i;) t+=(s+i)%(n+1) != 1;
+        cout<<t;
+    return 0;
+}
