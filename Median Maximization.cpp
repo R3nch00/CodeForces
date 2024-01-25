32f8a23e6bee88b8cfcc3dffa51e1b72ec31762c
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-01-26 03:16:39 +0600

    Add files via upload

---
+++ Median Maximization.cpp
@@ -0,0 +1,14 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#define ll long long
+#define ar array
+#define f(n) for(int i{};i<n;i++)
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n,s; cin>>n>>s;
+        cout<<s/(n/2+1)<<'\n';
+    }
+  return 0;
+}
