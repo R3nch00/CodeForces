a4986152841194ff9b893fab466f34c3cff658ea
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-01-30 00:27:47 +0600

    Add files via upload

---
+++ Divisible Array.cpp
@@ -0,0 +1,15 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#define ll long long
+#define ar array
+#define f(m) for(int i{};i<m;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main(){
+    io;
+    int T; cin>>T;
+    while(T--){
+        int N; cin>>N;
+    for(int i=1;i<=N;i++) cout<<2*i<<'\n';
+  }
+}
