5407f0ca173f5f9af5ab96e1bfaa5dd49c2aeab2
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-01-31 00:45:41 +0600

    Add files via upload

---
+++ Madoka and Math Dad.cpp
@@ -0,0 +1,15 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#define ll long long
+#define ar array
+#define f(n) for(int i{};i<n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main(){
+    int n,t; cin>>t;
+    while(t-->0){ cin>>n;
+        if(n%3==1) cout<<1;
+    for(int i=0;i<n/3;i++) cout<<21;
+        if(n%3==2)cout<<2; cout<<"\n"; }
+}
+
