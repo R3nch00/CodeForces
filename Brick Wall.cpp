1b22b9aceb8fbec22379e66a071e84b70f7d8e80
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-01 01:46:44 +0600

    Add files via upload

---
+++ Brick Wall.cpp
@@ -0,0 +1,16 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#define ll long long
+#define ar array
+#define ve vector
+#define f(n) for(int i{};i<n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int n,m,t;
+int main(){
+    io;
+	cin>>t;
+	while(t--){
+      cin>>n>>m; cout<< n*(m/2) <<"\n";
+    }
+}
