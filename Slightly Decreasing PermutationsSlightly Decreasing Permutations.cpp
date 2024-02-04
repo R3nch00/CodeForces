02dfb8ef1f32e041767460144ae1b5aa5d5f91da
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-04 23:24:38 +0600

    Add files via upload

---
+++ Slightly Decreasing PermutationsSlightly Decreasing Permutations.cpp
@@ -0,0 +1,17 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define pb push_back
+#define f(n) for(int i{};i<n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    io;
+    int n,k,i; cin>>n>>k;
+    for(i=1;i<n-k;i++) cout<<i<<" ";
+    for(i=n;i>=n-k;i--) cout<<i<<" ";
+}
