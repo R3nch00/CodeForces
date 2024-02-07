ecf12f454bf38467b8100d8297d564d22850660f
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-08 02:46:57 +0600

    Add files via upload

---
+++ Captain Flint and a Long Voyage.cpp
@@ -0,0 +1,21 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define pb push_back
+#define f(n) for(int i{};i<n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    io;
+    int q; cin>>q;
+    while(q--){
+        int n; cin>>n; int x=(n+3)/4;
+    for(int i{};i<n-x;++i) cout<<9;
+    for(int i{};i<x;++i) cout<<8;
+ cout<<endl;
+    }
+}
