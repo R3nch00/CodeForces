acbe1d2be4ee0751e43bad206a43efa3983d7cc5
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-12 04:06:08 +0600

    Add files via upload

---
+++ Bun Lover.cpp
@@ -0,0 +1,21 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Os")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define pb push_back
+#define f(x) for(int i{};i<x;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    io;
+    int t; cin>>t;
+    while(t--){
+        ll a; cin>>a;
+        cout<<(a+3)*(a-1)+5<<"\n";
+  }
+ return 0;
+}
