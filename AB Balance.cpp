b050c66899ba55377bf302c4ff4f180dbdfa776c
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-14 02:01:58 +0600

    Add files via upload

---
+++ AB Balance.cpp
@@ -0,0 +1,22 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Os")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define pb push_back
+#define bl bool
+#define f(n) for(int i=1;i<=n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    io;
+    ll n; cin>>n;
+    while(n--){
+        st x; cin>>x;
+        x[0]=x.back();
+  cout<<x<<endl;
+ }
+}
