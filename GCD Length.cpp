4b1294bd851e719035a906ac05713ae37b970151
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-18 16:41:12 +0600

    Add files via upload

---
+++ GCD Length.cpp
@@ -0,0 +1,24 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Os")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define ch char
+#define pb push_back
+#define bl bool
+#define f(n) for(int i=1;i<=n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int a,b,c,t;
+using namespace std;
+signed main(){
+    io;
+    cin>>t;
+    while(t--){
+        cin>>a>>b>>c;
+        int x=pow(10,a-1)+pow(10,c-1), y=pow(10,b-1);
+      cout<<x<<" "<<y<<endl;
+  }
+}
