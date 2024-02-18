9d72b66397b409efa1c957680644973cd3739611
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-18 16:38:06 +0600

    Add files via upload

---
+++ Robot Program.cpp
@@ -0,0 +1,24 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Os")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define ch char
+#define pb push_back
+#define bl bool
+#define f(n) for(int i=1;i<=n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    io;
+    int t; cin>>t;
+    while(t--){
+        int a,b ; cin>>a>>b;
+        int c=max(a,b)*2-1;
+      if(a==b) c++;
+        cout<<c<<endl;
+    }
+}
