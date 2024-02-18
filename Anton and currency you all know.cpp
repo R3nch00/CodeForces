1b4f2698b61da8b65cad7192aa434ef8cefd4103
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-19 01:06:04 +0600

    Add files via upload

---
+++ Anton and currency you all know.cpp
@@ -0,0 +1,27 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Os")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define ch char
+#define pb push_back
+#define bl bool
+#define f(n) for(int i{};i<n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    io;
+    st s; cin>>s;
+    int n=s.size(),f{},p{};
+    f(n){
+      if((s[i]-'0')%2==0){
+        p=i;f=1;
+    if(s[i]<s[n-1]){ break; }
+      }
+    }
+  swap(s[n-1],s[p]);
+  cout<<(f ? s : "-1");
+}
