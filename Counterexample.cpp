d3fc1a638a55832b831a197828a893d123811551
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-20 02:11:52 +0600

    Add files via upload

---
+++ Counterexample.cpp
@@ -0,0 +1,22 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Os")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define ch char
+#define pb push_back
+#define bl bool
+#define f(n) for(int i{};i<n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    io;
+    ll l,r; cin>>l>>r;
+    if(l%2!=0) l++;
+    if(abs(l-r)<2){ cout<<-1<<endl; }
+    else cout<<l<<" "<<l+1<<" "<<l+2<<endl;
+    return 0;
+}
