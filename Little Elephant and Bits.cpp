08482d4d2902583a485af61f156ef871ea2a7b21
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-02-20 03:00:35 +0600

    Add files via upload

---
+++ Little Elephant and Bits.cpp
@@ -0,0 +1,22 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Os")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define ch char
+#define pb push_back
+#define bl bool
+#define f(n) for(int i{};i<n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    io;
+    st s; cin>>s;
+    int p=s.find('0');
+    s.erase(s.begin()+(p == -1 ? 0 : p));
+    cout<<s;
+  return 0;
+}
