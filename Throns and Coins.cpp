02444d960e0938067b1aca4c47387c07485f0cd7
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-03-10 02:03:56 +0600

    Add files via upload

---
+++ Throns and Coins.cpp
@@ -0,0 +1,26 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Os")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define ch char
+#define pb push_back
+#define bl bool
+#define f(n) for(int i{};i<n;i++)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    io;
+    int t; cin>>t;
+    while(t--){
+        int n,c=0; st s; cin>>n>>s;
+      f(n){
+        if(s[i]=='@') c++;
+        else if(s[i]=='*' && s[i+1]=='*') break;
+      }
+   cout<<c<<endl;
+ }
+}
