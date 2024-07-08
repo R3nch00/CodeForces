0cc68b90461521d81d878b42419e8211f52fc2cc
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-07-08 23:12:01 +0600

    Add files via upload

---
+++ Stair, Peak, or Neither.cpp
@@ -0,0 +1,27 @@
+// mes que un club //
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Os")
+#define ll long long
+#define st string
+#define ve vector
+#define ar array
+#define ch char
+#define pb push_back
+#define bl bool
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define lb lower_bound
+#define ub upper_bound
+#define f(n) for(int i{};i<n;++i)
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+signed main(){
+    int t; cin>>t;
+    while(t--){
+        int a,b,c; cin>>a>>b>>c;
+      puts((a<b && b<c) ? "STAIR" : (a<b && b>c) ? "PEAK" : "NONE");
+    }
+}
