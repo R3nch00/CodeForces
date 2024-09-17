69f4d65f155fdb2f2c0655445b176708cec73532
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2024-09-18 01:30:18 +0600

    Add files via upload

---
+++ Legs.cpp
@@ -0,0 +1,10 @@
+// mes que un club //
+#include<bits/stdc++.h>
+using namespace std;
+signed main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        cout<<n/4+(n%4)/2<<endl;
+    }
+}
